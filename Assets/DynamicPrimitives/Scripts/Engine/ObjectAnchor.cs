de584351e2cc1fbc8648573e90e41ef9e1beac28
Song Young-jin <myevan80@gmail.com>
2015-05-03 13:58:59 +0900

    + 오브젝트 앵커

---
+++ Assets/DynamicPrimitives/Scripts/Engine/ObjectAnchor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ObjectAnchor : MonoBehaviour
+{
+    public RectTransform canvasTransform;
+
+    public void SetTargetTransform(Transform targetTransform)
+    {
+        _targetTransform = targetTransform;
+    }
+
+    private void Awake()
+    {
+        _cachedRectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (_targetTransform == null)
+            return;
+
+        var screenPoint = RectTransformUtility.WorldToScreenPoint(
+            Camera.main, _targetTransform.position);
+
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvasTransform, screenPoint, Camera.main, out localPoint);
+
+        _cachedRectTransform.anchoredPosition = localPoint;
+    }
+
+    private Transform _targetTransform;
+    private RectTransform _cachedRectTransform;
+}
